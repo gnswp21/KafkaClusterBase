4b7fef504c45a717a9e2125a99c7dc74d95d37bc
family <gnswp21@gmail.com>
2024-07-26 23:37:08 +0900

    FEAT k8s

--- conf/kafka/kafka-init.sh
+++ conf/kafka/kafka-init.sh
@@ -9,16 +9,16 @@ done
 echo 'sleep 20sec to activate zookeeper normally'
 sleep 20
 
-for i in 1 2 3; do
+for i in 0 1 2; do
   host="kafka-$i.kafka-svc-domain.default.svc.cluster.local"
   ssh $host "sh /root/kafka/bin/kafka-server-start.sh -daemon /root/kafka/config/server.properties"
 done
 
 
-for i in 1 2 3; do
-  host="kafka-$i.kafka-svc-domain.default.svc.cluster.local"
-  echo $host
-  ssh $host jps -m
-  echo "ls /brokers/ids" | /root/kafka/bin/zookeeper-shell.sh $host:2181
-done
+#for i in 0 1 2; do
+#  host="kafka-$i.kafka-svc-domain.default.svc.cluster.local"
+#  echo $host
+#  ssh $host jps -m
+#  echo "ls /brokers/ids" | /root/kafka/bin/zookeeper-shell.sh $host:2181
+#done
 
