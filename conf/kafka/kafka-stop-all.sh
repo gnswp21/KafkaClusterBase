ed5ea9ef1760149c6a8132b8c29799d26cf0c0b2
family <gnswp21@gmail.com>
2024-07-27 15:44:30 +0900

    FEAT almost

--- conf/kafka/kafka-stop-all.sh
+++
@@ -1,9 +0,0 @@
-for i in 1 2 3; do
-  host="mykafka$i"
-  ssh $host "sh /root/kafka/bin/kafka-server-stop.sh"
-done
-for i in 1 2 3; do
-  host="mykafka$i"
-  ssh $host "sh /root/kafka/bin/zookeeper-server-stop.sh"
-done
-
