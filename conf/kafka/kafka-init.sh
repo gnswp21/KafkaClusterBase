4a9e5495161f446e0289753a833b9ebe4702a586
family <gnswp21@gmail.com>
2024-07-27 16:16:57 +0900

    FEAT running kafka cluster by docker-compose.yml

--- conf/kafka/kafka-init.sh
+++ conf/kafka/kafka-init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Substitute environment variables in server.properties
-echo temp >> /root/kafka/config/server.properties
-echo ${KAFKA_ZOOKEEPER_CONNECT}
+envsubst < /root/kafka/config/server.properties > /root/kafka/config/server.properties.tmp
+mv /root/kafka/config/server.properties.tmp /root/kafka/config/server.properties
+
 # Start Kafka server
 exec /root/kafka/bin/kafka-server-start.sh /root/kafka/config/server.properties
